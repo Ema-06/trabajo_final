389d654f0f693397b3af94cc4ed46b790cd978c3
Facundo Ciruzzi <facundo-c-c@proton.me>
2026-05-15 20:59:20 -0300

    commit

---
+++ tp5_22.py
@@ -0,0 +1,15 @@
+def buscarPrimeraPosicion(palabra: str, letra: str) -> int | None:
+
+    for i in range(len(palabra)):
+        if letra == palabra[i]:
+            return i
+
+
+letra = input("Ingrese una letra: ")
+palabra = input("Ingrese una palabra: ")
+posicion = buscarPrimeraPosicion(palabra, letra)
+
+if posicion is not None:
+    print("la lerea " + letra + " esta en la posicion: " + str(posicion))
+else:
+    print("No se encontro dentro de la palabra")
